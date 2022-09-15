5d5d1c64cdacc58f40e9fd9a84fd9966d7f92f27
Vinicius Egidio <me@vinicius.io>
2022-09-15 07:09:16 +0200

    ⚡️ Completed the ORM for Countries table

--- src/main/kotlin/io/vinicius/tplspring/country/Country.kt
+++ src/main/kotlin/io/vinicius/tplspring/country/Country.kt
@@ -1,28 +1,72 @@
 package io.vinicius.tplspring.country
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import org.hibernate.annotations.Type
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "countries")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Country(
     @Id
     val code: String,
 
     @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
-    val name: CountryName
-)
+    val name: Name,
 
-data class CountryName(
-    val common: String,
-    val official: String,
-    val nativeName: List<CountryNativeName>
-)
+    @Column(nullable = true)
+    val capital: String?,
 
-data class CountryNativeName(
-    val language: String,
-    val common: String,
-    val official: String
-)
+    val region: String,
+
+    @Column(name = "sub_region", nullable = true)
+    val subRegion: String?,
+
+    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
+    val languages: List<Language>,
+
+    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
+    val currencies: List<Currency>,
+
+    val population: Int,
+    val area: Float,
+
+    @Type(type = "list-array")
+    @Column(columnDefinition = "real[]")
+    val coordinates: List<Float>,
+
+    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
+    val flags: Flag
+) {
+    data class Name(
+        val common: String,
+        val official: String,
+        val nativeName: List<NativeName>
+    )
+
+    data class NativeName(
+        val language: String,
+        val common: String,
+        val official: String
+    )
+
+    data class Language(
+        val code: String,
+        val name: String
+    )
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    data class Currency(
+        val code: String,
+        val name: String,
+        val symbol: String?
+    )
+
+    data class Flag(
+        val png: String,
+        val svg: String
+    )
+}
