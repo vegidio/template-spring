6986f85744f897b28458364a0d31b90199ee9431
Vinicius Egidio <me@vinicius.io>
2026-02-13 19:14:46 +0100

    refactor: Remove unused schema annotation and add interceptor for trailing slash redirection

--- src/main/kotlin/io/vinicius/tplspring/config/WebConfig.kt
+++ src/main/kotlin/io/vinicius/tplspring/config/WebConfig.kt
@@ -1,10 +1,33 @@
 package io.vinicius.tplspring.config
 
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
-    // Note: setUseTrailingSlashMatch has been removed in Spring Boot 4.0
-    // Trailing slash matching is now disabled by default
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(TrailingSlashRedirectInterceptor())
+    }
+
+    /**
+     * Redirects URLs with trailing slashes to the same URL without the trailing slash.
+     */
+    class TrailingSlashRedirectInterceptor : HandlerInterceptor {
+        override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
+            val uri = request.requestURI
+
+            if (uri.length > 1 && uri.endsWith("/")) {
+                val urlWithoutSlash = uri.dropLast(1)
+                val redirectUrl = request.queryString?.let { "$urlWithoutSlash?$it" } ?: urlWithoutSlash
+                response.sendRedirect(redirectUrl)
+                return false
+            }
+
+            return true
+        }
+    }
 }
--- src/main/kotlin/io/vinicius/tplspring/model/Response.kt
+++ src/main/kotlin/io/vinicius/tplspring/model/Response.kt
@@ -1,13 +1,9 @@
 package io.vinicius.tplspring.model
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import io.swagger.v3.oas.annotations.media.Schema
 
 @JvmRecord
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Response<T>(
     val data: T? = null,
-
-    @field:Schema(hidden = true)
-    val error: Error? = null,
 )
